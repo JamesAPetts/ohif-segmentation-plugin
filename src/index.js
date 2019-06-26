fe09c76d2081d3d17c03907e924492ce10d7b130
James Petts <jpetts@RLE-EECS-MTL-DHCP-31-64.MIT.EDU>
2019-06-26 17:45:44 -0400

    re-enabled ROIContour panel

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import init, { TOOL_NAMES } from './init.js';
 import SegmentationMenu from './components/segmentationMenu/SegmentationMenu.js';
-// import RoiContourMenu from './components/roiContourMenu/RoiContourMenu.js';
-import Dummy from './components/Dummy.js';
+import RoiContourMenu from './components/roiContourMenu/RoiContourMenu.js';
+// import Dummy from './components/Dummy.js';
 
 const TOOLBAR_BUTTON_TYPES = {
   COMMAND: 'command',
@@ -122,7 +122,7 @@ export default {
         },
         {
           id: 'contour-panel',
-          component: Dummy
+          component: RoiContourMenu
         }
       ],
       defaultContext: ['VIEWER']
